31613947f4f7ac319121b4ee9dc255fe7c758d78
ayushmandwivedi <ayushman14dwivedi@gmail.com>
2025-05-04 18:21:45 +0530

    sets up base and configures basic themes and colors for project

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="min-h-screen">
+      {/* Theme toggle */}
+      {/* Background effects */}
+      {/* Navbar */}
+      {/* Main Content */}
+      {/* Footer */}
+    </div>
+  );
+};
+
+export default Home;
