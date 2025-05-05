9a81c737f1553c00d764a962d650533db11646c5
ayushmandwivedi <ayushman14dwivedi@gmail.com>
2025-05-06 04:29:28 +0530

    adds contact section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ import HeroSection from "../components/HeroSection";
 import AboutMe from "../components/AboutMe";
 import SkillsSection from "../components/SkillsSection";
 import ProjectsSection from "../components/ProjectsSection";
+import ContactSection from "../components/ContactSection";
 
 const Home = () => {
   return (
@@ -22,6 +23,7 @@ const Home = () => {
         <AboutMe />
         <SkillsSection />
         <ProjectsSection />
+        <ContactSection />
       </main>
       {/* Footer */}
     </div>
